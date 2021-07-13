0668dac284308fb7efac2fc176a5fca165a37c0c
Ibnu Hazar <ibnuhazar101@gmail.com>
2021-07-13 13:40:10 +0700

    adjust page thankyou/pre-order

--- pages/thankyou/pre-order/index.vue
+++ pages/thankyou/pre-order/index.vue
@@ -2,43 +2,55 @@
   <div class="payment">
     <div class="container">
       <div class="row">
-        <div class="col">
-          <div class="payment__img">
-            <img src="/images/thank-you.png" alt="Image not found" />
+        <div
+          class="col md:bg-white tn:mt-12 tn:-mb-12 md:mb-0 md:mt-24 md:rounded-2xl md:shadow-xl tn:p-4 tn:py-8 md:p-12 text-center"
+        >
+          <div class="payment__img md:w-3/5 mx-auto">
+            <img
+              class="w-full"
+              src="/images/thank-you.png"
+              alt="Image not found"
+            />
           </div>
           <h3 class="payment-thankyou text-2xl font-bold mt-10 text-center">
             Thank You!
           </h3>
 
-          <p>
+          <p class="my-8 md:w-3/5 mx-auto">
             Terima kasih telah melakukan pendaftaran.
             <br />Paket ini adalah paket Pre-Order dimana akun akan dibuat
             setelah member dalam satu grup telah terkumpul
             {{ setNumberMember(provider) }} orang.
           </p>
-          <div class="box">
-            <div class="row">
-              <div class="col box-title">Provider</div>
+          <div
+            class="box md:w-4/5 lg:w-3/5 xl:w-2/5 mx-auto border-primary rounded p-6"
+          >
+            <div class="grid grid-cols-5">
+              <div class="col box-title text-left font-bold">Provider</div>
               <div class="col col-lg-1">:</div>
-              <div class="col box-item">{{ setNameProvider(provider) }}</div>
+              <div class="col box-item col-span-3 text-left">
+                {{ setNameProvider(provider) }}
+              </div>
             </div>
-            <div class="row mt-1">
-              <div class="col box-title">Paket</div>
+            <div class="grid grid-cols-5 mt-1">
+              <div class="col box-title text-left font-bold">Paket</div>
               <div class="col col-lg-1">:</div>
-              <div class="col box-item">{{ packet }}</div>
+              <div class="col box-item col-span-3 text-left">{{ packet }}</div>
             </div>
-            <div class="row mt-1">
-              <div class="col box-title">Harga</div>
+            <div class="grid grid-cols-5 mt-1">
+              <div class="col box-title text-left font-bold">Harga</div>
               <div class="col col-lg-1">:</div>
-              <div class="col box-item">{{ formatMoneyRupiah(total) }}</div>
+              <div class="col box-item col-span-3 text-left">
+                {{ formatMoneyRupiah(total) }}
+              </div>
             </div>
           </div>
-          <p>
+          <p class="my-6 font-bold text-blue-500">
             <a href="/info/pre-order" target="_blank">
               <i>Baca ketentuan Pre-Order selengkapnya</i>
             </a>
           </p>
-          <p>
+          <p class="md:w-3/5 mx-auto">
             Pembayaran akan dilakukan setelah satu grup full dan akun berhasil
             dibuat.
             <br />
@@ -46,7 +58,9 @@
             Pastikan nomor Whatsapp kamu aktif, kamu akan dihubungi oleh Admin
             melalui Whatsapp untuk proses selanjutnya.
             <br />Hubungi Admin
-            <a href="https://api.whatsapp.com/send?phone=6282124852227"
+            <a
+              class="font-bold text-blue-500"
+              href="https://api.whatsapp.com/send?phone=6282124852227"
               >+6282124852227</a
             >
           </p>
@@ -178,140 +192,4 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-.payment {
-  padding: 30px 40px 10px !important;
-
-  .container {
-    background: white;
-    border-radius: 10px;
-    box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.08);
-  }
-  .box {
-    border: 1px solid #86d0c1;
-    border-radius: 4px;
-    padding: 16px;
-    max-width: 26rem;
-    margin: 0 auto;
-  }
-  .col {
-    text-align: center;
-
-    &.box {
-      &-title {
-        text-align: left;
-        font-weight: 700;
-        max-width: 7rem;
-      }
-      &-item {
-        text-align: left;
-        &-noRek {
-          cursor: pointer;
-          text-align: left;
-          &:hover {
-            color: #86d0c1;
-          }
-        }
-      }
-    }
-    h3 {
-      margin-top: 20px !important;
-      margin-bottom: 20px !important;
-      font-weight: 700;
-    }
-    p {
-      margin: 0 auto;
-      max-width: 600px;
-      margin-top: 30px;
-      margin-bottom: 40px;
-    }
-    a {
-      font-weight: 700;
-      color: #2895ff;
-    }
-  }
-  .container {
-    max-width: 1120px !important;
-    font-weight: 500 !important;
-    margin: 0 auto !important;
-  }
-  &__img {
-    img {
-      width: 40rem;
-    }
-  }
-  .row {
-    padding: 0px 8px !important;
-  }
-  #snackbar {
-    background-color: #daeeef;
-    color: #2f524b;
-    text-align: center;
-    border-radius: 4px;
-    padding: 16px;
-    position: fixed;
-    z-index: 1;
-    top: 100px;
-    font-size: 17px;
-    margin: 0 auto;
-    max-width: 600px;
-    left: 65%;
-    margin-left: -300px;
-    font-weight: 400;
-    display: grid;
-    button {
-      margin-top: 0px !important;
-      margin-bottom: 10px !important;
-    }
-    span {
-      font-size: 28px;
-      font-weight: 700;
-      cursor: pointer;
-      padding: 0px 12px;
-    }
-  }
-}
-@media (max-width: 800px) {
-  #snackbar {
-    position: absolute !important;
-    max-width: 200px !important;
-    left: 30% !important;
-    top: 60% !important;
-    margin-left: 0px !important;
-  }
-  .payment {
-    padding: 50px 0px 0px !important;
-    // margin-top: 18px;
-    Ï &__img {
-      text-align: center;
-      margin-top: 20px;
-    }
-    &__header {
-      &-h3 {
-        font-size: 22px;
-        margin-top: 16px;
-      }
-    }
-    img {
-      width: 330px !important;
-      margin: 16px auto;
-    }
-    h2 {
-      font-size: 20px;
-    }
-    .box {
-      margin-left: 0px !important;
-      margin-right: 0px !important;
-      font-size: 13px;
-    }
-    .col-lg-1 {
-      max-width: 10px;
-    }
-  }
-}
-@media only screen and (min-width: 880px) and (max-width: 1020px) {
-  .container {
-    display: contents !important;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
